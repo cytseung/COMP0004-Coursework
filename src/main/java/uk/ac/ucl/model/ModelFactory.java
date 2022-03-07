373bc6a79d43602af491b6fe7272ef12d25e3dd7
cyt <NA>
2022-03-07 18:03:13 +0000

    initial commit

---
+++ src/main/java/uk/ac/ucl/model/ModelFactory.java
@@ -0,0 +1,15 @@
+package uk.ac.ucl.model;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ModelFactory {
+    private static Model model;
+    public static Model getModel() throws IOException {
+        if (model == null){
+            model = new Model();
+            model.readFile(new File("./data/patients.csv"));
+        }
+        return model;
+    }
+}
